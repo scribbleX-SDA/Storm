26c97e8aeedad91d719771ee936efbb416cf5dc2
ScribbleX <101875601+scribbleX-SDA@users.noreply.github.com>
2022-11-01 12:51:07 +0530

    slx

--- javascript/src/privacyCheck.js
+++
@@ -1,9 +0,0 @@
-var privacyStat_count = 1;
-function privacyStat(){
-    privacyStat_count += 1;
-    if(privacyStat_count%2 == 0){
-        document.getElementById("togglePrivateBtn").value = "PRIVATE";
-    }else{
-        document.getElementById("togglePrivateBtn").value = "PUBLIC";
-    }
-}
